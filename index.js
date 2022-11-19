9039ee362e39a4855885230b133f80381b811a32
edyta0106 <edyta0106@gmail.com>
2022-11-19 16:59:45 -0700

    worked on nodejs in index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Provide a short description explaining the what, why, and how of your project.",
+    // What was your motivation?
+    // Why did you build this project?
+    // What problem does it solve?
+    // What did you learn?
+    name: "description",
+  },
+  {
+    type: "input",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide instructions and examples for use. Include screenshots as needed.",
+    name: "usage",
+  },
+  {
+    type: "list",
+    choices: ["MIT", "Apache", "BSD"],
+    message: "Please select a license",
+    name: "license",
+  },
+  {
+    //   How do I give the user the option to skip over this question?
+    type: "input",
+    message:
+      "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.",
+    name: "contribution",
+  },
+  {
+    //   How do I give the user the option to skip over this question?
+    type: "input",
+    message: "Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Please enter your GitHub username.",
+    // How to add a link to GitHub profile?
+    name: "question1",
+  },
+  {
+    type: "input",
+    message: "Please enter your email.",
+    //  How to provide instructions on how to reach me?
+    name: "question2",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.log(err) : console.log("success");
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    const fileData = generateMarkdown(response);
+    writeToFile("README.md", fileData);
+  });
+}
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![License](https://img.shields.io/badge/license-${license}-blue)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license != "none") {
+    return `[License](#license)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != "none") {
+    return `## License
+    ${license}`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * ${renderLicenseLink(data.license)}
+  * [How to Contribute](#contribution)
+  * [Test Instructions](#tests)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
+  ## How to Contribute
+  ${data.contribution}
+  ## Test Instruction
+  ${data.tests}
+  ## Contact Me
+  // email and LinkedIn will be created via a renderEmail and renderLinkedIn functions that will be posted below. Still need to figure out how to add instructions on how to reach user with additional questions.
+  ${data.question1}
+  ${data.question2}
 `;
 }
 
